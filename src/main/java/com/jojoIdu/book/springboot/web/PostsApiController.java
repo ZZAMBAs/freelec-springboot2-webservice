6b60d03e82dce795407ce6c6ed784022945a2687
ZZAMBAs <rksidksrksi@naver.com>
2021-02-09 20:44:55 +0900

    오타수정

--- src/main/java/com/jojoIdu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoIdu/book/springboot/web/PostsApiController.java
@@ -15,6 +15,6 @@ public class PostsApiController {
 
     @PostMapping("api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
-        return PostsService.save(requestDto);
+        return postsService.save(requestDto);
     }
 }
